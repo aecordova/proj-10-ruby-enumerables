0529d9f7565068d2273086ae2b57d932b6135d17
aecordova <ae_cordova@outlook.com>
2020-01-16 15:31:03 -0600

    Added my_all and my_any methods

--- my_enumerable_module.rb
+++ my_enumerable_module.rb
@@ -26,16 +26,34 @@ module Enumerable
     end
     result
   end
+
+  def my_any?
+    arr = self # this is to prevent a redundant-use-of-self error
+    response = true
+    0.upto(arr.length - 1) do |i|
+      response = false unless yield(arr[i])
+    end
+    response
+  end
+  def my_any?
+    arr = self # this is to prevent a redundant-use-of-self error
+    response = false
+    0.upto(arr.length - 1) do |i|
+      response = true if yield(arr[i])
+    end
+    response
+  end
+
   
-  def all; end
 end
 # tests
 a = [1, 2, 3, 4, 5, 6, 7]
 
 # a.my_each { |v| print 'value ' + v.to_s }
 # a.my_each_with_index { |v, k| print 'value ' + v.to_s + ' index ' + k.to_s }
-
-print a.my_select{ |x| x % 2 == 0 }
+# print a.my_select{ |x| x % 2 == 0 }
+# puts a.my_all?{ |x| x > 2 }
+# puts a.my_any?{ |x| x > 4 }
 
 # my_select in the same way, though you may use #my_each in your definition
 # my_all? (continue as above)
