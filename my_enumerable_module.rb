d9947e7f6cdc5904567a74291c27351d0caa8537
aecordova <ae_cordova@outlook.com>
2020-01-16 15:04:38 -0600

    Added my_select method

--- my_enumerable_module.rb
+++ my_enumerable_module.rb
@@ -1,26 +1,41 @@
 # frozen_string_literal: true
 
+# Extending Enumerable module with replicated methods for the following:
+# each, each_with_index, select,
+#
 module Enumerable
-  def my_each()
-    0.upto(self.size-1) do |i|
-      yield(self[i])
+  def my_each
+    arr = self # this is to prevent a redundant-use-of-self error
+    0.upto(arr.length - 1) do |i|
+      yield(arr[i])
     end
   end
 
-  def my_each_with_index()
-    0.upto(self.size-1) do |i|
-      yield(self[i],i)
+  def my_each_with_index
+    arr = self # this is to prevent a redundant-use-of-self error
+    0.upto(arr.length - 1) do |i|
+      yield(arr[i], i)
     end
   end
 
+  def my_select
+    arr = self # this is to prevent a redundant-use-of-self error
+    result = []
+    0.upto(arr.length - 1) do |i|
+      result << arr[i] if yield(arr[i])
+    end
+    result
+  end
+  
+  def all; end
 end
+# tests
+a = [1, 2, 3, 4, 5, 6, 7]
 
+# a.my_each { |v| print 'value ' + v.to_s }
+# a.my_each_with_index { |v, k| print 'value ' + v.to_s + ' index ' + k.to_s }
 
-a=[1,2,3,4,5,6,7]
-
-a.my_each{ |v| puts 'value ' + v.to_s}
-a.my_each_with_index(){ |v, k| puts 'value ' + v.to_s + ' index '+k.to_s }
-
+print a.my_select{ |x| x % 2 == 0 }
 
 # my_select in the same way, though you may use #my_each in your definition
 # my_all? (continue as above)
