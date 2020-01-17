11c03e92c55ef54dfc512b1541b702cd2b0367d5
aecordova <ae_cordova@outlook.com>
2020-01-17 17:44:00 -0600

    Added my_inject and modified my_map to accept both a proc or a block also wrote test cases at the end

--- my_enumerable_module.rb
+++ my_enumerable_module.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Extending Enumerable module with replicated methods for the following:
-# each, each_with_index, select,
+# each, each_with_index, select, all?, none?, any?, count, map and inject
 #
 module Enumerable
   def my_each
@@ -55,7 +55,7 @@ module Enumerable
   end
 
   def my_count(single_value = nil)
-    # arr = self
+    arr=self
     counter = 0
     if single_value
       arr.my_each { |val| counter += 1 if val == single_value }
@@ -65,26 +65,76 @@ module Enumerable
     counter
   end
 
-  def my_map
+  def my_map(a_proc = nil)
     arr = self # this is to prevent a redundant-use-of-self error
-    arr.my_each_with_index do |v,i|
-      arr[i] = yield(v)
+    if block_given? && a_proc.nil?
+      arr.my_each_with_index { |v, i| arr[i] = yield(v) }
+    else
+      arr.my_each_with_index { |v, i| arr[i] = a_proc.call(v) }
     end
     arr
   end
 
+  # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+  def my_inject(init = 0, oper = :+)
+    obj = self
+    if init.is_a? Symbol
+      oper = init
+      init = 0
+    end
+    accum = obj[init]
+    if block_given?
+      (init + 1).upto(obj.length - 1) { |i| accum = yield(accum, obj[i]) }
+    else
+      (init + 1).upto(obj.length - 1) { |i| accum = accum.send(oper, obj[i]) }
+    end
+    accum
+  end
+  # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 end
-# tests
-a = [1, 2, 3, 4, 5, 6, 7]
 
-# a.my_each { |v| print 'value ' + v.to_s }
-# a.my_each_with_index { |v, k| print 'value ' + v.to_s + ' index ' + k.to_s }
-# print a.my_select{ |x| x % 2 == 0 }
-# puts a.my_all? { |x| x > 2 }
-# puts a.my_any?{ |x| x > 4 }
-# puts a.my_none? { |x| x > 1 }
-# puts a.my_count { |x| x > 4 }
-# puts a.my_count(3)
-# puts a.my_map { |x| x ** 4 }
-
-# my_inject
+# TESTS
+# print "\n my_each: \n"
+# [1,2,3,4,5,6,7].my_each { |v| print "  val: #{v}\n" }
+#
+# print "\n my_each_with_index: \n"
+# [1,2,3,4,5,6,7].my_each_with_index { |v, k| print "  val: #{v}  ind: #{k} \n" }
+#
+# print "\n my_select: "
+# print [1,2,3,4,5,6,7].my_select{ |x| x % 2 == 0 }
+#
+# print "\n\n my_all?: "
+# puts [1,2,3,4,5,6,7].my_all? { |x| x > 2 }
+#
+# print "\n my_any?: "
+# puts [1,2,3,4,5,6,7].my_any?{ |x| x > 4 }
+#
+# print "\n my_my_none?: "
+# puts [1,2,3,4,5,6,7].my_none? { |x| x > 1 }
+#
+# print "\n my_count using a block:"
+# puts [1,2,3,4,5,6,7].my_count { |x| x > 4 }
+#
+# print "\n my_count using an argument:"
+# puts [1,2,3,4,5,6,7].my_count(3)
+#
+# print "\n my_map using a block:"
+# puts [1,2,3,4,5,6,7].my_map { |x| x ** 4 }
+#
+# print "\n my_inject using a block:"
+# puts [1,2,3,4,5,6,7].my_inject { |running_total, number| running_total + number }
+#
+# print "\n my_inject using an  operator as a symbol:"
+# puts [1,2,3,4,5,6,7].my_inject(:*)
+#
+# print "\n Defining and using multiply_els for testing my_inject: "
+# def multiply_els(arr)
+#   arr.my_inject(:*)
+# end
+# puts multiply_els([2, 4, 5])
+# print "\n Defining a proc to be used with my_map: \n"
+# some_proc = Proc.new { |x| x ** 4 }
+#
+# print "\n Using my_map with a proc and with both a prc and a block: \n"
+# puts [1,2,3,4,5,6,7].my_map(some_proc)
+# puts [1,2,3,4,5,6,7].my_map() { |x| x ** 4 }
