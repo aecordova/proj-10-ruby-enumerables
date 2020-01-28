ba94bf92550b465100fd0a7fd5c568e374fd684f
acordova <ae_cordova@outlook.com>
2020-01-28 10:43:25 -0600

    Created test cases for my_each_with_index

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,4 +1,4 @@
-require_relative './my_enumerable_module.rb'
+require_relative '../my_enumerable_module.rb'
 
 describe Enumerable do
   describe "#my_each" do
@@ -7,6 +7,14 @@ describe Enumerable do
     end
     it "Returns an enumerable if block is not given" do
       expect([5, 6, 7].my_each.class).to eql(Enumerator)
+    end
+  end
+  describe "#my_each_with_index" do
+    it "Returns the same array if block is given" do
+      expect([5, 6, 7].my_each_with_index{|x,y| x+y}).to eql([5, 6, 7])
+    end
+    it "Returns an enumerable if block is not given" do
+      expect([5, 6, 7].my_each_with_index.class).to eql(Enumerator)
     end
   end
 end
